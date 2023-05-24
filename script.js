62bc0c18e960a69e713721fcf6521aae4df5d058
Victor Manuel Bonilla Escobar <victmanuel04.06@gmail.com>
2023-05-24 13:11:46 -0500

    Encrypt functions

--- script.js
+++ script.js
@@ -28,24 +28,60 @@ form.addEventListener('submit', function(event) {
 
 // Caesar Cipher encryption function
 function caesarCipherEncrypt(text) {
-    // Add your Caesar Cipher encryption logic here
-    // Return the encrypted text
+    const shift = 3;
+    let encryptedText = '';
+    
+    for (let i = 0; i < text.length; i++) {
+        let charCode = text.charCodeAt(i);
+        
+        if (charCode >= 65 && charCode <= 90) {
+            // Uppercase letter
+            charCode = ((charCode - 65 + shift) % 26) + 65;
+        } else if (charCode >= 97 && charCode <= 122) {
+            // Lowercase letter
+            charCode = ((charCode - 97 + shift) % 26) + 97;
+        }
+        
+        encryptedText += String.fromCharCode(charCode);
+    }
+    
+    return encryptedText;
 }
 
 // Vigenère Cipher encryption function
 function vigenereCipherEncrypt(text) {
-    // Add your Vigenère Cipher encryption logic here
-    // Return the encrypted text
+    const keyword = 'KEY'; // Change this to your desired keyword
+    const keywordLength = keyword.length;
+    let encryptedText = '';
+    
+    for (let i = 0; i < text.length; i++) {
+        let charCode = text.charCodeAt(i);
+        const keywordCharCode = keyword.charCodeAt(i % keywordLength);
+        
+        if (charCode >= 65 && charCode <= 90) {
+            // Uppercase letter
+            charCode = ((charCode - 65 + keywordCharCode - 65) % 26) + 65;
+        } else if (charCode >= 97 && charCode <= 122) {
+            // Lowercase letter
+            charCode = ((charCode - 97 + keywordCharCode - 65) % 26) + 97;
+        }
+        
+        encryptedText += String.fromCharCode(charCode);
+    }
+    
+    return encryptedText;
 }
 
 // AES Encryption function
 function aesEncrypt(text) {
     // Add your AES Encryption logic here
     // Return the encrypted text
+    return 'AES encrypted text';
 }
 
 // Rail Fence Cipher encryption function
 function railFenceCipherEncrypt(text) {
     // Add your Rail Fence Cipher encryption logic here
     // Return the encrypted text
+    return 'Rail Fence encrypted text';
 }
